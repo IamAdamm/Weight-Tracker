e663f85061fb19f30bdeb9db418c99ee3ce53e3e
Adam <adam.astamir@hotmail.de>
2024-04-13 21:29:10 +0200

    Update weight.js

--- Weight Tracker/weight.js
+++ Weight Tracker/weight.js
@@ -2,31 +2,109 @@
 let weightbtn = document.getElementById('weightButton')
 let caloriesbtn = document.getElementById('caloriesButton')
 let submitbtn = document.getElementById("Submit")
+let gainradio = document.getElementById('gainradio')
+let lossradio = document.getElementById('lossradio')
+let firstCard = document.getElementById('welcomeCard')
+let thirdCard = document.getElementById('card')
+let secondCard = document.getElementById('goalsetcard')
+let startinginput = document.getElementById('startinginput')
+let goalsetbtn = document.getElementById('goalsetbtn')
 
 //arrays for storing weight and kcal
 let weightArray = []
 let caloriesArray = []
 
-//submit button function
+let startingWeight = []
+let goalWeight = []
+
+//dynamically hide the card
+thirdCard.classList.add('hidden')
+secondCard.classList.add('hidden')
+
+//button function for first card
+function goal(){
+    if(gainradio.checked || lossradio.checked) {
+        firstCard.classList.add('hidden')
+        secondCard.classList.remove('hidden')
+    }
+}
+
+//button function for second card
+
+function setGoal(){
+
+    // Convert the values to numbers
+    let startinginputValue = parseFloat(startinginput.value);
+    let goalinputValue = parseFloat(goalinput.value)
+
+    // Check if the input is a number and less than 200 but more than 40
+    if(typeof startinginputValue === 'number' && startinginputValue < 200 && startinginputValue > 40){
+        startinginput.classList.add('hidden')
+    } else {
+        startinginput.classList.add('shake')
+        startinginput.classList.add('invalid')
+        setTimeout(() => {
+            startinginput.classList.remove('shake')
+        }, 500);
+    }
+
+    if(typeof goalinputValue === 'number' && goalinputValue < 200 && goalinputValue > 40){
+        goalinput.classList.add('hidden')
+    } else {
+        goalinput.classList.add('shake')
+        goalinput.classList.add('invalid')
+        setTimeout(() => {
+            goalinput.classList.remove('shake')
+        }, 500);
+}
+    //if both are valid inputs continue to next card and store the inputs
+    if(goalinput.classList.contains('hidden') && startinginput.classList.contains('hidden')) {
+        goalWeight.push(goalinputValue)
+        startingWeight.push(startinginputValue)
+        secondCard.classList.add('hidden')
+        thirdCard.classList.remove('hidden')
+}
+}
+
+//button function for third card
 function submit(){
+
     // Convert the values to numbers
     let weightValue = parseFloat(weightbtn.value);
     let caloriesValue = parseFloat(caloriesbtn.value);
 
-    // Check if the weight is a number and less than 200
+    // Check if the input is a number and less than 200
     if(typeof weightValue === 'number' && weightValue < 200){
         weightArray.push(weightValue)
+        weightbtn.classList.add('hidden')
 } else {
-    //insert shake animation------------
-    alert('Please enter your correct Weight')
+    weightbtn.classList.add('shake')
+    weightbtn.classList.add('invalid')
+    setTimeout(() => {
+        weightbtn.classList.remove('shake')
+    }, 500);
 }
+
     // Check if the calories are a number and more than 200
     if(typeof caloriesValue === 'number' && caloriesValue > 200){
         caloriesArray.push(caloriesValue)
+        caloriesbtn.classList.add('hidden')
 } else {
-    //insert shake animation------------
-    alert('Please enter your correct calorie count')
-}
+    caloriesbtn.classList.add('shake')
+    caloriesbtn.classList.add('invalid')
+    setTimeout(() => {
+        caloriesbtn.classList.remove('shake')
+    }, 500);
 }
 
+    //if both are valid inputs continue to next card
+    if(caloriesbtn.classList.contains('hidden') && weightbtn.classList.contains('hidden')) {
+        submitbtn.classList.add('hidden')
+    }
+}
+
+//Events
+gainradio.addEventListener('click', goal)
+lossradio.addEventListener('click', goal)
 submitbtn.addEventListener("click", submit);
+goalsetbtn.addEventListener('click', setGoal)
