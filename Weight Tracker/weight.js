a2242499c396e29578bc6d448c122d1fcddcdde2
Adam <adam.astamir@hotmail.de>
2024-04-14 19:00:01 +0200

    Update weight.js

--- Weight Tracker/weight.js
+++ Weight Tracker/weight.js
@@ -7,8 +7,11 @@ let lossradio = document.getElementById('lossradio')
 let firstCard = document.getElementById('welcomeCard')
 let thirdCard = document.getElementById('card')
 let secondCard = document.getElementById('goalsetcard')
+let fourthCard = document.getElementById('statcard')
 let startinginput = document.getElementById('startinginput')
 let goalsetbtn = document.getElementById('goalsetbtn')
+let progress = document.getElementById('progress')
+let counter = 0
 
 //arrays for storing weight and kcal
 let weightArray = []
@@ -17,9 +20,13 @@ let caloriesArray = []
 let startingWeight = []
 let goalWeight = []
 
-//dynamically hide the card
+let goalinputValue;
+
+//dynamically hide the cards
 thirdCard.classList.add('hidden')
 secondCard.classList.add('hidden')
+fourthCard.classList.add('hidden')
+
 
 //button function for first card
 function goal(){
@@ -35,10 +42,10 @@ function setGoal(){
 
     // Convert the values to numbers
     let startinginputValue = parseFloat(startinginput.value);
-    let goalinputValue = parseFloat(goalinput.value)
+    goalinputValue = parseFloat(goalinput.value)
 
     // Check if the input is a number and less than 200 but more than 40
-    if(typeof startinginputValue === 'number' && startinginputValue < 200 && startinginputValue > 40){
+    if(typeof startinginputValue === 'number' && startinginputValue < 200 && startinginputValue > 40 && startinginputValue !== goalinputValue){
         startinginput.classList.add('hidden')
     } else {
         startinginput.classList.add('shake')
@@ -48,7 +55,7 @@ function setGoal(){
         }, 500);
     }
 
-    if(typeof goalinputValue === 'number' && goalinputValue < 200 && goalinputValue > 40){
+    if(typeof goalinputValue === 'number' && goalinputValue < 200 && goalinputValue > 40 && startinginputValue !== goalinputValue){
         goalinput.classList.add('hidden')
     } else {
         goalinput.classList.add('shake')
@@ -59,7 +66,9 @@ function setGoal(){
 }
     //if both are valid inputs continue to next card and store the inputs
     if(goalinput.classList.contains('hidden') && startinginput.classList.contains('hidden')) {
-        goalWeight.push(goalinputValue)
+        goalWeight.shift;
+        startingWeight.shift;
+        goalWeight.push(goalinputValue);
         startingWeight.push(startinginputValue)
         secondCard.classList.add('hidden')
         thirdCard.classList.remove('hidden')
@@ -99,9 +108,23 @@ function submit(){
 
     //if both are valid inputs continue to next card
     if(caloriesbtn.classList.contains('hidden') && weightbtn.classList.contains('hidden')) {
-        submitbtn.classList.add('hidden')
+        thirdCard.classList.add('hidden')
+        fourthCard.classList.remove('hidden')
     }
 }
+
+//Progress Bar Counter function
+setInterval(() => {
+    if(counter === 65){
+        clearInterval()
+    } else {
+    counter += 1;
+    progress.innerHTML = counter + '%'
+}},30.77)
+
+//Progress card goal weight display
+document.getElementById("goalweightdisplay").innerHTML = goalinputValue;
+
 
 //Events
 gainradio.addEventListener('click', goal)
